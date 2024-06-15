5d6d4da7224c7068718715e500354c07a5606338
Obada-JL <leader.obada@gmail.com>
2024-06-15 11:55:45 +0300

    a

--- Controllers/unisController.js
+++ Controllers/unisController.js
@@ -1,22 +1,9 @@
 const University = require("../models/unisModel");
 
-function parseDate(dateStr) {
-  const [day, month, year] = dateStr.split(".").map(Number);
-  return new Date(year, month - 1, day); // Month is 0-indexed in JavaScript Date
-}
-
-// Function to sort universities by endApply date
-function sortUniversitiesByEndApply(universityArray) {
-  return universityArray.sort(
-    (a, b) => parseDate(a.endApply) - parseDate(b.endApply)
-  );
-}
-
 const getUnis = async (req, res) => {
   try {
     const unis = await University.find();
-    const sortedUnis = sortUniversitiesByEndApply(unis);
-    res.json(sortedUnis);
+    res.json(unis);
   } catch (error) {
     console.error("Error fetching universities:", error);
     res.status(500).json({ message: "Server error" });
